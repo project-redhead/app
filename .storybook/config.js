f59124caeb9e9634381f4f0d715b362a16b5f8a0
Gino Messmer <git@ginomessmer.me>
2019-06-04 11:12:46 +0200

    Add Storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure, addParameters } from '@storybook/react';
+
+addParameters({
+  backgrounds: [
+    { name: 'light', value: '#fff', default: true },
+    { name: 'dark', value: '#373737' },
+  ],
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
